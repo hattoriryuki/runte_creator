341a81a34221c8c6c93c865cd02685976f16fd08
kellyuka <yukamohri0719@yahoo.co.jp>
2022-03-20 15:48:26 +0900

    ADD:user/newにtailwind実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user), notice: 'User was successfully created.'
+      redirect_to root_path, notice: 'User was successfully created.'
     else
       render :new
     end
