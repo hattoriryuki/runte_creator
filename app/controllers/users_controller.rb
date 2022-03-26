b3fcae127d643551024f3ad4d8c7c3c15ab66115
kellyuka <yukamohri0719@yahoo.co.jp>
2022-03-26 22:28:10 +0900

    FIX:余分なファイルの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-
-  def index
-    @users = User.all
-  end
-
-  def show; end
-
   def new
     @user = User.new
   end
-
-  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -22,24 +12,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    if @user.update(user_params)
-      redirect_to user_url(@user), notice: 'User was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @user.destroy!
-    redirect_to users_url, notice: 'User was successfully destroyed.'
-  end
-
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
