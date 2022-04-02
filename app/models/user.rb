a56a3dc56b87ec1f4fb175f13fa0aae71860f53c
kellyuka <yukamohri0719@yahoo.co.jp>
2022-04-02 09:12:30 +0900

    ADD:likesテーブル作成、アソシエーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :pictures, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
