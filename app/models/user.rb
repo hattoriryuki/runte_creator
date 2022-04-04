b79ffd61ade2eb04fc98638f8f829866fe365bc1
佐藤祐美加 <satoyumika@satouyumikanoMacBook-Pro.local>
2022-04-04 23:17:51 +0900

    sorcery.rbファイルで設定を編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :pictures, dependent: :destroy
+  accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
