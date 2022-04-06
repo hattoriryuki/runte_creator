e3da847b10f2507fae14490144ca97f043f48df8
At-105ori84 <onex3komainu@gmail.com>
2022-04-06 13:46:56 +0900

    Improve: users, user_sessionsのflashメッセージを国際化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'ユーザーを作成しました。'
+      redirect_to root_path, notice: t('.success')
     else
-      flash.now[:danger] = 'ユーザー作成に失敗しました。'
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
