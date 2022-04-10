0f24f948912c0c165a800e96d26deb4170338a9b
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-04-10 11:07:43 +0900

    add: picturesのcontrollerとviewを実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,14 @@
 class PicturesController < ApplicationController
   def new
+    @picture = Picture.new
   end
 
   def create
+    @picture = Picture.new(image: params[:picture])
+    @picture.save
   end
 
   def index
+    @pictures = Picture.all
   end
 end
