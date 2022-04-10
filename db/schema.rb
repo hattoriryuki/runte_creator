ed7d3c5b120d7313a5b4bca7002540a3e64e9b87
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-04-10 10:33:00 +0900

    add: rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_235707) do
+ActiveRecord::Schema.define(version: 2022_04_10_011947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_04_01_235707) do
   create_table "pictures", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "picture_comment"
-    t.string "image", null: false
+    t.text "image", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_pictures_on_user_id"
