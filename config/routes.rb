44e33367cdaa4b206e22591151f8428fc1ebce80
佐藤祐美加 <satoyumika@satouyumikanoMacBook-Pro.local>
2022-04-14 15:36:35 +0900

    Add: ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  post "oauth/callback", to: "oauths#callback"
+  get "oauth/callback", to: "oauths#callback"
+  get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
 end
