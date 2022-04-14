2a757ece479838dddd84c341957b33518d7bd067
kellyuka <yukamohri0719@yahoo.co.jp>
2022-04-14 09:50:41 +0900

    ADD:ユーザー紹介作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
