c5849ba02b4685ad11423a4ebfed32523c3ee722
佐藤祐美加 <satoyumika@satouyumikanoMacBook-Pro.local>
2022-04-14 20:27:17 +0900

    Fix:sorcery.rbのアバター消去

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,23 +1,21 @@
 class OauthsController < ApplicationController
   skip_before_action :require_login, raise: false
-
   def oauth
-    return redirect_to root_path, info: t('defaults.info') if logged_in?
     login_at(params[:provider])
   end
 
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
-      redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+      redirect_to root_path
     else
       begin
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+        redirect_to root_path
       rescue
-        redirect_to root_path, error: "#{provider.titleize}アカウントでのログインに失敗しました。"
+        redirect_to root_path
       end
     end
   end
