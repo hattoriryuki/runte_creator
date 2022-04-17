327f5a7241a763064a588b01a6c9ba5674fe2753
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-04-17 18:33:13 +0900

    add: userモデルにself.guestを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { minimum: 2, maximum: 15 }
+
+  def self.guest_login
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'guest_user'
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+    end
+  end
 end
