0d93489328f0f397961693cf5c5a778b2d2c775d
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-04-17 18:30:24 +0900

    add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show]
   resources :users, only: %i[new create]
   resources :pictures, only: %i[index]
+
+  post 'guest', to: 'user_sessions#guest_login'
+  
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
