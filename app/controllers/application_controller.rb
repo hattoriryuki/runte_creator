631a6d950140dfd77a81447947f7df268d2f4abc
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-04-20 01:48:23 +0900

    add: 403エラーを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :render_403
+
+  def render_403
+    render file: Rails.root.join('public/403.html'), status: :forbidden, layout: false, content_type: 'text/html'
+  end
 end
