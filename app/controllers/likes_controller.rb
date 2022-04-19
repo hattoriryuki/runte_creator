d7bb707b6c4330564f51789d267f19f96c67ce84
shutorm02 <shutorm2106@gmail.com>
2022-04-19 13:51:21 +0900

    Add: likesコントローラーにcreateメソッドを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,7 @@
 class LikesController < ApplicationController
   def create
+    picture = Picture.find(params[:picture_id])
+    current_user.like(picture)
+    redirect_back fallback_location: root_path, success: t('.success')
   end
 end
