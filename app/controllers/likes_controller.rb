859a5090c8919a5f0a8892651a3fbf10c6e77b47
shutorm02 <shutorm2106@gmail.com>
2022-04-20 13:37:24 +0900

    Add: いいねボタンをajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,11 @@
 class LikesController < ApplicationController
   def create
-    picture = Picture.find(params[:picture_id])
-    current_user.like(picture)
-    redirect_back fallback_location: root_path, notice: t('.success')
-    @like_count = Like.where(picture_id: params[:picture_id]).count
+    @picture = Picture.find(params[:picture_id])
+    current_user.like(@picture)
   end
 
   def destroy
-    picture = current_user.likes.find(params[:id]).picture
-    current_user.unlike(picture)
-    redirect_back fallback_location: root_path, notice: t('.success')
-    @like_count = Like.where(picture_id: params[:picture_id]).count
+    @picture = current_user.likes.find(params[:id]).picture
+    current_user.unlike(@picture)
   end
 end
