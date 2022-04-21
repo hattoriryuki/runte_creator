9d136974e021e698298e2c3c7e0faed2e14f6ef6
miloneko <satohannn@gmail.com>
2022-04-22 00:47:53 +0900

    Add: ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'static_pages#top'
+
   resources :users, only: %i[new create show]
   resource :profile, only: %i[show edit update]
   resources :pictures, only: %i[new create index]
+  resources :password_resets, only: %i[new create edit update]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
