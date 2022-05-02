051b7ddb1ceab3076536cdd7569c5c09985e8ae7
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-02 22:42:29 +0900

    add: user_showにページネーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @pictures = @user.pictures
+    @pictures = @user.pictures.page(params[:page])
   end
 
   def create
