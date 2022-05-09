a02c4e909ce729af68d8d1af2bc7aae70bc87b6d
shutorm02 <shutorm2106@gmail.com>
2022-05-09 11:36:59 +0900

    Add: yarnでflowbiteを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin')
+  ],
+  content: [
+      "./node_modules/flowbite/**/*.js"
+    ]
 }
