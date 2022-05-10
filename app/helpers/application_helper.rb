836076a94da64b1938de6eefe8ab9c27bed0470c
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-10 21:41:43 +0900

    add: default_meta_tagsのメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,26 @@ module ApplicationHelper
   def flash_class(type)
     { danger: 'red', notice: 'green' }.fetch(type.to_sym)
   end
+
+  def default_meta_tags
+    {
+      title: '勉強で行き詰まった時にらんてくんを描いて、やすらぎと笑いを与え、現実逃避できるサービスです',
+      description: '勉強で行き詰まった時にらんてくんを描いて、やすらぎと笑いを与え、現実逃避できるサービスです',
+      keywords: 'らんてくん, お絵描き, プログラミング初学者, RUNTEQ',
+      charset: 'UTF-8',
+      og: {
+        title: 'RunteCreator',
+        description: 'RunteCreator',
+        type: 'website',
+        url: [request.original_url, 'http://127.0.0.1:3000'],
+        image: '#',
+        locale: "ja_JP"
+      },
+      twitter: {
+        site: '@runtecreator',
+        card: 'summary_large_image',
+        image: '#'
+      },
+    }
+  end
 end
