9f2b13b50716e57819a04f4b046013bea5e1a619
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-13 01:06:39 +0900

    add: ApplicationHelperにmeta-tags,OGPの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,26 @@ module ApplicationHelper
   def flash_class(type)
     { danger: 'red', notice: 'green' }.fetch(type.to_sym)
   end
+
+  def default_meta_tags
+    {
+      title: 'RunteCreator',
+      description: '勉強で行き詰まった時にらんてくんを描いて、やすらぎと笑いを与え、現実逃避できるサービスです',
+      keywords: 'らんてくん, お絵描き, RUNTEQ',
+      charset: 'UTF-8',
+      og: {
+        title: 'RunteCreator',
+        description: 'RunteCreator',
+        type: 'website',
+        url: [request.original_url, 'http://127.0.0.1:3000'],
+        image: asset_url('04_a.png'),
+        locale: "ja_JP"
+      },
+      twitter: {
+        site: '@runtecreator',
+        card: 'summary',
+        image: asset_url('04_a.png')
+      },
+    }
+  end
 end
