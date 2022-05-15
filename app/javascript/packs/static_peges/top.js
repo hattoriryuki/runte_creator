0c30e6123aff0217b9bf668b55dd41bb0c6909c3
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-16 03:43:07 +0900

    add: swiperのJavaScriptを追加

--- app/javascript/packs/static_peges/top.js
+++ app/javascript/packs/static_peges/top.js
@@ -1,10 +1,14 @@
 $(function(){
   $('.js-modal-open').on('click',function(){
       $('.js-modal').fadeIn();
+      $(".swiper").addClass("hidden");
       return false;
   });
   $('.js-modal-close').on('click',function(){
       $('.js-modal').fadeOut();
+      $('.swiper').delay(250).queue(function() {
+        $(this).removeClass('hidden').dequeue();
+      });
       return false;
   });
 });
