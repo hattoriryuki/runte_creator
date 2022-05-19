d903260981b7a117ee953b40e56fea61f3a7d361
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-20 01:29:03 +0900

    fix: switper.jsのコメントアウトを削除

--- app/javascript/packs/swiper/swiper.js
+++ app/javascript/packs/swiper/swiper.js
@@ -1,11 +1,9 @@
 window.addEventListener('DOMContentLoaded', function(){
   const swiper = new Swiper(".swiper", {
     spaceBetween: 50,
-    // ページネーションが必要なら追加
     pagination: {
       el: ".swiper-pagination"
     },
-    // ナビボタンが必要なら追加
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
