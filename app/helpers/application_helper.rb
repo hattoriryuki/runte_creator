cdc5b18de97c54eecae06ce317f7c665686cc3a3
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-19 23:21:39 +0900

    fix: ogp用の画像を設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,13 @@ module ApplicationHelper
         description: 'RunteCreator',
         type: 'website',
         url: [request.original_url, 'http://127.0.0.1:3000'],
-        image: asset_url('04_a.png'),
+        image: asset_url('ogp_runtequn.png'),
         locale: "ja_JP"
       },
       twitter: {
         site: '@runtecreator',
         card: 'summary',
-        image: asset_url('04_a.png')
+        image: asset_url('ogp_runtequn.png')
       },
     }
   end
