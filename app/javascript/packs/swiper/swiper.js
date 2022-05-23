e8a1a0abc0db4bc26c61ea23efd6d8009dbcfaaa
hattori ryuki <ryuki.97913795@i.softbank.jp>
2022-05-23 21:49:57 +0900

    fix: swiperをループでスライドできるように修正

--- app/javascript/packs/swiper/swiper.js
+++ app/javascript/packs/swiper/swiper.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function(){
   const swiper = new Swiper(".swiper", {
+    loop: true,
     spaceBetween: 50,
     pagination: {
       el: ".swiper-pagination"
