8bd42a9c535e01ce75d2a01191cd6cb2c6295517
miloneko <satohannn@gmail.com>
2022-05-24 08:59:32 +0900

    Fix: likesコントローラーの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,6 @@
 class LikesController < ApplicationController
-  before_action :set_picture, only: [:create]
   def create
-    @like = Like.create(user_id: current_user.id, picture_id: params[:picture_id])
-  end
-
-  private
-
-  def set_picture
     @picture = Picture.find(params[:picture_id])
+    current_user.like(@picture)
   end
 end
